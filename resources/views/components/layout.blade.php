96530b2f8662c15fb3089e53248c12b410f72f1e
RayzerDev <rayzergamer62@outlook.fr>
2023-12-29 12:34:58 +0100

    ajout d'une balise meta pour la responsivité

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="shortcut icon" href="https://portfolio.rayzerdev.fr/images/logo/logo_icone.png">
 
     @vite(['resources/css/portfolio.css'])
