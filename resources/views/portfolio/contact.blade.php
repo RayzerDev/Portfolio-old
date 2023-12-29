0d97736ca322f001c388c0339e00ab30f7c58c71
RayzerDev <rayzergamer62@outlook.fr>
2023-12-29 02:20:18 +0100

    Ajout d'un bouton pour télécharger le cv via la page contact

--- resources/views/portfolio/contact.blade.php
+++ resources/views/portfolio/contact.blade.php
@@ -2,7 +2,15 @@
     <div id="container">
         <div id="infos">
             <h2>Contactez-moi !</h2>
-            <h3>Mail: </h3><a href="mailto:louis.karamucki@outlook.fr">louis.karamucki@outlook.fr</a>
+            <div id="mail">
+                <h3>Mail: </h3><a href="mailto:louis.karamucki@outlook.fr">louis.karamucki@outlook.fr</a>
+            </div>
+            <a id="download" href="{{asset("storage/CV_Louis_KRMK.pdf")}}">
+            <div id="download-button">
+                <img src="{{asset("images/icons/download.svg")}}" alt="imgDown">
+                <p>Retrouvez mon cv</p>
+            </div>
+            </a>
         </div>
     </div>
 </x-layout>
