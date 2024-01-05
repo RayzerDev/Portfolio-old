1d63154dc4de7cb4de2d11af85feb08198fe74b8
RayzerDev <rayzergamer62@outlook.fr>
2024-01-05 23:07:21 +0100

    Changement dans les passions du a propos

--- resources/views/portfolio/about.blade.php
+++ resources/views/portfolio/about.blade.php
@@ -78,7 +78,7 @@
                 Je m'entraîne également à la salle de sport. J'aime jouer au foot avec mes amis, et supporter le RC Lens !</p>
             <p>Depuis très jeune, je suis plongé dans l'univers de la musique et j'en suis devenu guitariste. J'ai d'ailleurs fait quelques concerts.
                 C'est les Red Hot Chili Peppers qui m'ont inspiré, notamment John Frusciante.</p>
-            <p>Je joue pendant mes heures perdues à l'ordinateur,
+            <p>Je joue pendant mes heures perdues sur l'ordinateur,
                 et quand me vient une idée je développe comme ce portfolio.</p>
         </div>
     </div>
